4a385556c7a5fd358ed01160fe5a562b8d999df1
Sixolile Mtengwana <mtengwanas@gmail.com>
2018-12-28 16:01:50 +0200

    Dropdown
    + CSS Component (sun-<class_name> |suffix)
    - Event Emitter
    - Documentation

---
+++ src/app/@sunflower-module/sunflower-ui/ui-components/dropdown-menu/dropdown-menu.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'sun-dropdown-menu',
+  template: `
+  <div class="sun-dropdown"  [class.--focus]="open" (click)="draw()" (blur)="draw()">
+    <div class="sun-dropdown-item"  [class.--focus]="open">
+    {{ (title !== undefined)? title: item }}
+    </div>
+    <div class="sun-dropdown-container">
+    <ng-content></ng-content>
+      <div *ngFor="let item of items" class="sun-dropdown-item" [class.active]="item === title" (click)="select(item)" >
+        {{item}}
+      </div>
+      <div *ngIf="items === undefined || items === 0" class="sun-dropdown-item">
+       <i> _empty_ </i>
+      </div>
+    </div>
+  </div>
+  `
+})
+export class DropdownMenuComponent implements OnInit {
+@Input()
+  title:string;
+@Input()
+  items: string[]|number[]|any[];
+  item: string|number|any;
+
+  // Conditions
+   private open:boolean;
+
+
+  constructor() { 
+  }
+
+  ngOnInit() {    
+    if (this.title === undefined){
+      this.item, this.title =  (this.items!==undefined && this.items.length > 0)?this.items[0]:this.item;
+      
+    }
+  }
+
+  draw():void{
+    this.open = !this.open;
+  }
+
+  select(item:string|number|any):void{
+    this.item = item;
+  }
+
+  // The following are 
+
+}
+
+
+// actions
