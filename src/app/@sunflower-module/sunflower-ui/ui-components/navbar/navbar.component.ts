afa4685976e1370ae73b0f3727b32ff24865766e
Sixolile Mtengwana <mtengwanas@gmail.com>
2019-01-07 23:43:02 +0200

    Navbar
    + Responsive (no media tags)
    + paths attribute added
    + Documentation

---
+++ src/app/@sunflower-module/sunflower-ui/ui-components/navbar/navbar.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input, OnInit } from "@angular/core";
+import { Path } from "../../model/path.model";
+@Component({
+  selector: "sunflower-navbar",
+  template: `
+    <div class="navbar">
+      <div class="navbar-brand">
+        <img
+          src="./assets/img/sunflower/sunflower-fund-sunflower.png"
+          class="brand-img"
+          alt=""
+        />
+      </div>
+      <div class="navbar-brand-name">Sunflower Fund</div>
+      <div class="navbar-container">
+        <a *ngFor="let path of paths" class="navbar-item fa" [ngClass]="path.icon">{{ path.name }}</a>
+      </div>
+    </div>
+
+    <!-- 
+    This is code that renders the code for a button.
+    How it works:
+    
+      <sunflower-navbar [paths]="pathsArray">Button</sunflower-button>
+
+    The * paths * attribute is to pass an array links/paths described on the 
+    router module.
+
+    The * paths * attribute is an array that references the @sunflower-module models.
+
+    Note: Navigate to: @sunflower-module/sunflower-ui/model to see Path Reference
+    discription.
+ -->
+  `
+})
+export class NavbarComponent implements OnInit {
+  @Input()
+    paths:Path[];
+  
+  constructor() {}
+
+  ngOnInit() {}
+}
