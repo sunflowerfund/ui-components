9429cd1d688e9bcd3820f24be171baf106cd657f
Lihle Mdikili <Lihle@gmcowboyaliens.com>
2019-01-15 01:02:22 +0200

    navigating with indicators and Next Button

--- src/app/@sunflower-module/sunflower-ui/ui-components/circular-indicator/circular-indicator.component.ts
+++ src/app/@sunflower-module/sunflower-ui/ui-components/circular-indicator/circular-indicator.component.ts
@@ -4,22 +4,41 @@ import { Component, OnInit } from '@angular/core';
   selector: 'sun-indicator',
   template: `
     <ul>
-      <li class = "active indicator">1</li>
-      <li class = "indicator">2</li>
-      <li class = "indicator">3</li>
-      <li class = "indicator">4</li>
-      <li class = "indicator">5</li>
-      <li class = "indicator">6</li>
-      <li class = "indicator">7</li>
+      <li (click) = "setUpClass(1)" [class] = "class1">1</li>
+      <li (click) = "setUpClass(2)" [class] = "class2">2</li>
+      <li (click) = "setUpClass(3)" [class] = "class3">3</li>
     </ul>  
+    <button (click) = "setUpClass((index+1))">button</button>
   `,
 
 })
 export class CircularIndicatorComponent implements OnInit {
-
+  index:number = 1;
+  class1:string = "active indicator";
+  class2:string = "indicator";
+  class3:string = "indicator";
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setUpClass(index){
+    this.index = index;
+    if(index == 1){
+      this.class1 = "active indicator";
+      this.class2 = "indicator";
+      this.class3 = "indicator";
+    }
+    if(index == 2){
+      this.class1 = "indicator";
+      this.class2 = "active indicator";
+      this.class3 = "indicator";
+    }
+    if(index == 3){
+      this.class1 = "indicator";
+      this.class2 = "indicator";
+      this.class3 = "active indicator";
+    }
   }
 
 }
