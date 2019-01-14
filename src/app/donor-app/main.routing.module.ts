56d87af06a4836e49df189286c630dc73df0da2c
Lihle Mdikili <Lihle@gmcowboyaliens.com>
2019-01-14 11:38:10 +0200

    Dashboard component intial commit with routing

--- src/app/donor-app/main.routing.module.ts
+++ src/app/donor-app/main.routing.module.ts
@@ -13,6 +13,7 @@ import { HomePage } from "./pages/home/home.page";
 import { ComponentsPage } from './pages/components.page';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 
 const routes: Route[] = [
@@ -20,6 +21,7 @@ const routes: Route[] = [
   { path: "login", component:  SignInComponent},
   { path: "register", component: SignUpComponent },
   { path: "components", component: ComponentsPage },
+  { path: "dashboard", component: DashboardComponent },
   { path: "", redirectTo: "/", pathMatch: "full" }
 ];
 
@@ -31,6 +33,7 @@ const routes: Route[] = [
       //   { enableTracing: true } // <-- debugging purposes only
     )
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [DashboardComponent]
 })
 export class MainRoutingModule {}
