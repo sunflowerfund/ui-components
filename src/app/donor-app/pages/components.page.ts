b97d1336a0d350b540bffc0609b767792ee4f041
Nicholas Smith <lwandilenick@gmail.com>
2019-01-21 02:11:40 +0200

    Service worker added and tested and works
    added http-server module to make ngsw testing possible

--- src/app/donor-app/pages/components.page.ts
+++ src/app/donor-app/pages/components.page.ts
@@ -7,21 +7,21 @@ import { Component, OnInit } from '@angular/core';
 <h1>Sunflower Navbar:</h1>
 <sunflower-navbar></sunflower-navbar>
 
-<h1>Sunflower Fund Buttons:</h1>
+<<!-- h1>Sunflower Fund Buttons:</h1>
 
 <div style="max-width: fit-content;">
   <sunflower-button type="primary" [attrs]="[]" click="alert('Button Clicked!')">Register and become a donor!</sunflower-button>
 </div>
 
 <h1> Dropdown Menu</h1>
-<!-- The Custom Dropdon Button -->
-<!-- Container - Containing Dropdown -->
+The Custom Dropdon Button -->
+<!-- Container - Containing Dropdown 
 
 <div style="max-width: 243.78px;">
   <sun-dropdown-menu title="choose an item" type="primary" [attrs]="[]" [items]=" ['1st item','2nd item','3rd item']"
     (output)="getOption($event)"></sun-dropdown-menu>
 </div>
-
+-->
   `
 })
 export class ComponentsPage implements OnInit {
